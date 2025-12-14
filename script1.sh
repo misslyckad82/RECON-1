1596a743238dfda6ce7408e5c08ad9ab0a73d5b2
Sanna.Rytilahti <sanna.rytilahti@gmail.com>
2025-12-14 21:11:34 +0100

    Min första commit, lade till readme fil

---
+++ script1.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+# Detta script samlar in systeminformation - RECON
+#
+# Kan användas för följande attacker:
+# [Skriv möjliga attacker]
+#
+# Author: Frans Schartau
+# Last Update: 2025-01-01
+
+
+echo "Välkommen till RECON script för att kontrollera en Linux-miljö"
+
+echo
+echo "=== SYSTEMINFO ==="
+uname -a
+
+echo
+echo "=== AKTUELL ANVÄNDARE ==="
+echo $USER
+
+echo
+echo "=== INLOGGADE ANVÄNDARE ==="
+who
+
+echo
+echo "=== ANVÄNDARE MED SHELL ==="
+grep "sh$" /etc/passwd
+
+echo
+echo "=== NÄTVERK ==="
+ip a | grep inet
+
+echo
+echo "=== OPERATIVSYSTEM ==="
+cat /etc/os-release
+
+echo
+echo "=== RAM-ANVÄNDNING ==="
+free -h
+ 
+echo
+echo "=== CPU-ARKITEKTUR ==="
+lscpu
+
+echo
+echo "=== PROCESSER OCH RESURSANVÄNDNING ==="
+top
+
+echo
+echo "=== SHELL-KONTON ==="
+cat /etc/passwd grep "sh$"
+
+echo
+echo "=== PUBLIK IP- OCH PLATSINFO ==="
+curl https://ipapi.co/json/
+
+echo
+echo "=== ÖPPNA PORTAR ==="
+ss -tuln
+
+echo
+echo "=== SLUT PÅ RECON SCRIPT ==="
+
+
+ 
+# skriv in dina egna kommandon för tester
+
+
+
+
+
